a0cc4987825b2488aef3cef47aa700e89e444589
Ron Gierlach <rongierlach@gmail.com>
2017-06-06 11:02:45 -0500

    adjust Socials styles

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,19 +5,19 @@ import CodePenSVG from './CodePenSVG'
 const Socials = () => (
   <section className='pv4 ph3 tc'>
     <a
-      className='black grow dib mh3'
+      className='near-black grow dib mh3'
       href='https://www.codepen.io/youfoundron'
       target='_blank'
       title='CodePen'
     ><CodePenSVG /></a>
     <a
-      className='black grow dib mh3'
+      className='near-black grow dib mh3'
       href='https://github.com/rongierlach'
       target='_blank'
       title='GitHub'
     ><GitHubSVG /></a>
     <a
-      className='black grow dib mh3'
+      className='near-black grow dib mh3'
       href='https://www.linkedin.com/in/rongierlach'
       target='_blank'
       title='LinkedIn'
