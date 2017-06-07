b2d8dca61095d1a44448f85a04c96cdcf711380a
Ron Gierlach <rongierlach@gmail.com>
2017-06-07 10:40:47 -0500

    remove unnecessary wrapWithProvider util

--- src/util/wrapWithProvider.js
+++
@@ -1,8 +0,0 @@
-import { Provider } from 'react-redux'
-import store from '../store'
-
-export default Component => props => (
-  <Provider store={store}>
-    <Component {...props} />
-  </Provider>
-)
