3b88b877a2f58cc4a35cc317e43b7d8d4eaa3045
Ron Gierlach <rongierlach@gmail.com>
2017-06-06 19:53:55 -0500

    change work page copy

--- pages/work.js
+++ pages/work.js
@@ -11,9 +11,8 @@ const Work = () => (
     <section className='mw7-ns center'>
       <h1>Work</h1>
       <hr />
-      <p className='lh-copy'>Previous projects include work for Nike, Air Jordan, Anheuser-Busch, Chevy, Converse, VH1, Virgin, Activision, Barclaycard, Disney, Sonos + Spotify, Porsche, Behr, and Lollapalooza.</p>
+      <p className='lh-copy'>Previous projects include work for Nike, Disney, Anheuser-Busch, Chevy, Converse, VH1, Virgin, Activision, Barclaycard, Sonos + Spotify, Porsche, Behr, and Lollapalooza.</p>
       <p className='lh-copy'>As well as major artists such as Beyoncé, Modest Mouse, System of a Down, Demi Lovato, Carly Rae Jepsen, Sean Mendes, and many more.</p>
-      {/*  */}
     </section>
   </Layout>
 )
