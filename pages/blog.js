1d83e34b088aa3ce7b29c1417d9ac669196f8a18
Ron Gierlach <rongierlach@gmail.com>
2017-06-09 02:24:05 -0500

    put fade animations on the blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,34 +1,41 @@
 import Head from 'next/head'
-// import Link from 'next/link'
 
-// import CMS from '../src/util/CMS'
 import Layout from '../src/components/Layout.js'
+import FadeIn from '../src/components/FadeIn'
+import PostList from '../src/components/PostList'
+import CMS from '../src/util/CMS'
 import withRedux from '../src/util/withRedux'
 
-const Blog = () => (
+const Blog = ({ data }) => (
   <Layout>
     <Head>
       <title>Blog</title>
     </Head>
-    <section className='mw7-ns center'>
-      <h1>Blog</h1>
-      <hr />
-    </section>
-    {/* <PostList /> */}
+    <FadeIn id='blog' direction='up'>
+      <section className='mw7-ns center'>
+        <h1>Blog</h1>
+        <hr />
+        <PostList posts={data} />
+      </section>
+    </ FadeIn>
   </Layout>
 )
 
-// Blog.getInitialProps = async context => {
-  // const posts = []
-  // const {
-  //   page = 1,
-  //   page_size = 10,
-  //   query
-  // } = context.query
+Blog.getInitialProps = async ({query = {}}) => {
+  const {
+    page = 1,
+    page_size = 10, // eslint-disable-line camelcase
+    exclude_body = true, // eslint-disable-line camelcase
+    query: search,
+    ...params
+  } = query
+  const action = !search ? 'list' : 'search'
+  const args = !search
+    ? [{page, page_size, exclude_body, ...params}]
+    : [search, {page, page_size, exclude_body, ...params}]
 
-//   const action = !context.query ? 'list' : 'search'
-//   const res = await CMS.post[action](context.query)
-//   return res.data
-// }
+  const res = await CMS.post[action](...args)
+  return res.data
+}
 
 export default withRedux(Blog)
