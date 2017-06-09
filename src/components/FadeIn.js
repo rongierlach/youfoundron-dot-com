d8b034300ac6b8b1b2d9f2743a3081930a8c5dd9
Ron Gierlach <rongierlach@gmail.com>
2017-06-09 02:22:40 -0500

    write FadeIn component to handle most animations

---
+++ src/components/FadeIn.js
@@ -0,0 +1,94 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class FadeIn extends Component {
+  state = { activated: false }
+  delayTimeout = undefined
+
+  static propTypes = {
+    id: PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    delay: 0,
+    duration: '1s',
+    direction: ''
+  }
+
+  componentDidMount () {
+    this.delayTimeout = setTimeout(() => {
+      this.setState(state => ({activated: true}))
+    }, Number(this.props.delay))
+  }
+
+  componentWillUnount () {
+    window.clearTimeout(this.delayTimeout)
+  }
+
+  render () {
+    const { id, children, direction, duration } = this.props
+    const { activated } = this.state
+
+    return (
+      <div className={activated ? `${id}-activated` : `${id}-deactivated`}>
+        {children}
+        <style global jsx>{`
+          @keyframes fade-in {
+            0%   { opacity: 0 }
+            100% { opacity: 1 }
+          }
+          @keyframes fade-in-left {
+            0% {
+              opacity: 0;
+              transform: translateX(-20px)
+            }
+            100% {
+              opacity: 1;
+              transform: translateX(0)
+            }
+          }
+          @keyframes fade-in-right{
+            0% {
+              opacity: 0;
+              transform: translateX(20px)
+            }
+            100% {
+              opacity: 1;
+              transform: translateX(0)
+            }
+          }
+          @keyframes fade-in-up {
+            0% {
+              opacity: 0;
+              transform: translateY(20px)
+            }
+            100% {
+              opacity: 1;
+              transform: translateY(0)
+            }
+          }
+          @keyframes fade-in-down {
+            0% {
+              opacity: 0;
+              transform: translateY(-20px)
+            }
+            100% {
+              opacity: 1;
+              transform: translateY(0)
+            }
+          }
+        `}</style>
+        <style>{`
+          .${id}-activated {
+            animation-duration: ${typeof duration === 'number' ? `${duration}s` : duration};
+            animation-name: ${direction ? `fade-in-${direction}` : 'fade-in'};
+          }
+          .${id}-deactivated {
+            opacity: 0;
+            transition: ${typeof duration === 'number' ? `${duration}s` : duration};
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
