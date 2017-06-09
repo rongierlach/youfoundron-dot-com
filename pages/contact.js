da941287781ad01bdc13fcb028bfba94ff40afe6
Ron Gierlach <rongierlach@gmail.com>
2017-06-09 02:23:41 -0500

    put fade animations on contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,18 +2,22 @@ import Head from 'next/head'
 
 import withRedux from '../src/util/withRedux'
 import Layout from '../src/components/Layout'
+import FadeIn from '../src/components/FadeIn'
 import ContactForm from '../src/components/ContactForm'
 
 const Contact = () => (
   <Layout>
     <Head>
       <title>Contact</title>
+      {/* <script src='https://www.google.com/recaptcha/api.js' /> */}
     </Head>
-    <section className='measure center'>
-      <h1>Contact</h1>
-      <hr />
-      <ContactForm />
-    </section>
+    <FadeIn id='contact' direction='up'>
+      <section className='measure center'>
+        <h1>Contact</h1>
+        <hr />
+        <ContactForm />
+      </section>
+    </FadeIn>
   </Layout>
 )
 
