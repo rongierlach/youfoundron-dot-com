57d64d0749ab9f58f0599c234f50fdf8264eef70
Ron Gierlach <rongierlach@gmail.com>
2017-06-09 15:15:02 -0500

    add reducer for resetting and breakpointing nav

--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -6,8 +6,12 @@ const defaultState = selectNavStatus(initialState)
 
 export default (state = defaultState, action) => {
   switch (action.type) {
+    case types.SET_SMALL:
+      return state.set('isSmall', action.payload)
     case types.CLOSE_NAV:
       return state.set('expanded', false)
+    case types.RESET_NAV:
+      return state.set('expanded', undefined)
     case types.TOGGLE_NAV:
       return state.update('expanded', expanded => !expanded)
     default:
