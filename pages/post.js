16eefcf26d860a0572bdb8a57b86eebfed2a2e75
Ron Gierlach <rongierlach@gmail.com>
2017-06-10 11:54:23 -0500

    normalize post elements back from butter

--- pages/post.js
+++ pages/post.js
@@ -8,16 +8,24 @@ import FadeIn from '../src/components/FadeIn.js'
 import CMS from '../src/util/CMS'
 import withRedux from '../src/util/withRedux'
 
-// const selectionsWithClasses = [
-//   ['p', 'f5 f3-ns lh-copy'],
-//   ['blockquote', 'f6 f5-ns lh-copy i pl4 bl bw1 b--gold mb4'],
-//   ['a', 'link near-black hover-gold']
-// ]
+const elementClassNames = {
+  'p': 'f5 f4-ns lh-copy georgia',
+  'a': 'ml0 link near-black hover-gold',
+  'blockquote': 'ml0 f6 f5-ns lh-copy i pl4 bl bw1 b--gold mb4'
+}
+
+const normalizeElement = (i, elm) => {
+  const classNames = elementClassNames[elm.name]
+  if (classNames) {
+    const elmClass = elm.attribs.class || ''
+    elm.attribs.class = elmClass + ' ' + classNames
+  }
+  return elm
+}
 
 const normalizeBody = body => {
   const $ = cheerio.load(body)('body')
-  // console.log($.html())
-  return body
+  return $.contents().map(normalizeElement)
 }
 
 const Post = ({ error, post = {} }) => (
@@ -27,11 +35,17 @@ const Post = ({ error, post = {} }) => (
     <Head>
       <title>{post.seo_title}</title>
       <meta name='description' content={post.meta_description} />
+      <link rel='stylesheet' type='text/css' href='http://d2z2rr99bkshyr.cloudfront.net/buttercms-post-defaults.css' />
+      <style>{`
+        .align-center {
+          text-align: center;
+        }
+      `}</style>
     </Head>
     <article>
       <header className='bg-gold sans-serif'>
         <FadeIn id='header' direction='right' >
-          <div className='mw9 center pa4 pt5-ns ph4 ph5-m ph6-l'>
+          <div className='mw9 center pa4 pt5-ns ph4 ph7-l'>
             <time className='f6 mb2 dib ttu tracked'>
               <small>{m(post.published).format('DD MMMM, YYYY')}</small>
             </time>
@@ -49,12 +63,14 @@ const Post = ({ error, post = {} }) => (
           </div>
         </FadeIn>
       </header>
-      <FadeIn id='body' delay={500} direction='up'>
-        <div
-          className='bg-near-white pa4 ph7-l georgia mw9-l center measure'
-          dangerouslySetInnerHTML={{__html: normalizeBody(post.body)}}
-        />
-      </FadeIn>
+      <section className='mw9 center pa4 pt5-ns ph4 ph7-l'>
+        <FadeIn id='body' delay={500} direction='up'>
+          <div
+            className='bg-near-white georgia center'
+            dangerouslySetInnerHTML={{__html: normalizeBody(post.body)}}
+          />
+        </FadeIn>
+      </section>
     </article>
   </Layout>
 )
