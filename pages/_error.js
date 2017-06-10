ecb8252fdbb11f3378dc079d53a07dfa6d021d13
Ron Gierlach <rongierlach@gmail.com>
2017-06-10 10:02:00 -0500

    add statusText prop to Error component

--- pages/_error.js
+++ pages/_error.js
@@ -1,31 +1,27 @@
-import { Component } from 'react'
 import Head from 'next/head'
 
 import withRedux from '../src/util/withRedux'
 import Layout from '../src/components/Layout'
 import FadeIn from '../src/components/FadeIn'
 
-class Error extends Component {
-  static getInitialProps ({ res, jsonPageRes }) {
-    const statusCode = res ? res.statusCode : (jsonPageRes ? jsonPageRes.status : 404)
-    return { statusCode }
-  }
+const Error = ({statusCode, statusText}) => (
+  <Layout>
+    <Head>
+      <title>Error</title>
+    </Head>
+    <FadeIn id='error'>
+      <section className='measure center'>
+        <h1 className='tc tracked-tight f-headline lh-solid mb0'>{statusCode}</h1>
+        <p className='tc georgia i f5 f4-ns lh-copy'>{statusText}</p>
+      </section>
+    </FadeIn>
+  </Layout>
+)
 
-  render () {
-    return (
-      <Layout>
-        <Head>
-          <title>Error</title>
-        </Head>
-        <FadeIn id='error'>
-          <section className='measure center'>
-            <h1 className='tc tracked-tight f-headline lh-solid mb0'>{this.props.statusCode}</h1>
-            <p className='tc georgia i f5 f4-ns lh-copy'>Nothing to see here!</p>
-          </section>
-        </FadeIn>
-      </Layout>
-    )
-  }
+Error.getInitialProps = ({ res, jsonPageRes }) => {
+  const statusCode = res ? res.statusCode : (jsonPageRes ? jsonPageRes.status : 404)
+  const statusText = 'Nothing to see here!'
+  return { statusCode, statusText }
 }
 
 export default withRedux(Error)
