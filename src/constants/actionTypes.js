0bd2ec8fae090b9742a008389c64cab450d5a406
Ron Gierlach <rongierlach@gmail.com>
2017-06-11 11:09:23 -0500

    remove spreads from actionTypes

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,27 +1,38 @@
-const scopeTypes = (types = [], scope = 'App') => (
-  types.reduce(
-    (scopedTypes, type) => {
-      scopedTypes[type] = Symbol(`${scope}/${type}`)
-      return scopedTypes
-    }, {}
-  )
-)
-
-const appTypes = [
-  'SET_SMALL',
-  'CLOSE_NAV',
-  'RESET_NAV',
-  'TOGGLE_NAV'
-]
-
-const contactFormTypes = [
-  'CHANGE_NAME',
-  'CHANGE_EMAIL',
-  'CHANGE_MESSAGE',
-  'SUBMIT_FORM'
-]
+// const scopeTypes = (types = [], scope = 'App') => (
+//   types.reduce(
+//     (scopedTypes, type) => {
+//       scopedTypes[type] = Symbol(`${scope}/${type}`)
+//       return scopedTypes
+//     }, {}
+//   )
+// )
+//
+// const appTypes = [
+//   'SET_SMALL',
+//   'CLOSE_NAV',
+//   'RESET_NAV',
+//   'TOGGLE_NAV'
+// ]
+//
+// const contactFormTypes = [
+//   'CHANGE_NAME',
+//   'CHANGE_EMAIL',
+//   'CHANGE_MESSAGE',
+//   'SUBMIT_FORM'
+// ]
+//
+// export default {
+//   ...scopeTypes(appTypes),
+//   ...scopeTypes(contactFormTypes, 'ContactForm')
+// }
 
 export default {
-  ...scopeTypes(appTypes),
-  ...scopeTypes(contactFormTypes, 'ContactForm')
+  SET_SMALL: 'SET_SMALL',
+  CLOSE_NAV: 'CLOSE_NAV',
+  RESET_NAV: 'RESET_NAV',
+  TOGGLE_NAV: 'TOGGLE_NAV',
+  CHANGE_NAME: 'CHANGE_NAME',
+  CHANGE_EMAIL: 'CHANGE_EMAIL',
+  CHANGE_MESSAGE: 'CHANGE_MESSAGE',
+  SUBMIT_FORM: 'SUBMIT_FORM'
 }
