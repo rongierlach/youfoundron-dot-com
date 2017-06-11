8b2c64361682b2725dc8a11a011bf6ed8f716f7b
Ron Gierlach <rongierlach@gmail.com>
2017-06-10 23:37:09 -0500

    no plugins

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ module.exports = {
   webpack: (config, {dev}) => {
     // plugins
     const plugins = []
-    const dotEnv = dev
-      ? new DotEnv({safe: true})
-      : new EnvironmentPlugin({
-        'process.env.GA_TRACKING_ID': JSON.stringify(process.env.GA_TRACKING_ID),
-        'process.env.BUTTER_API_TOKEN': JSON.stringify(process.env.BUTTER_API_TOKEN)
-      })
-
-    plugins.push(dotEnv)
+    // const env = dev
+    //   ? new DotEnv({safe: true})
+    //   : new EnvironmentPlugin({
+    //     'process.env.GA_TRACKING_ID': JSON.stringify(process.env.GA_TRACKING_ID),
+    //     'process.env.BUTTER_API_TOKEN': JSON.stringify(process.env.BUTTER_API_TOKEN)
+    //   })
+    //
+    // plugins.push(env)
 
     config.plugins = config.plugins || []
     config.plugins.push(...plugins)
