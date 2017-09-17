8bcbaf302dfcd8ac4e0690194290eccb659b50d0
Ron Gierlach <rongierlach@gmail.com>
2017-09-17 03:43:00 -0500

    update layout for IE

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import Footer from './Footer'
 import withAnalytics from '../util/withAnalytics'
 
 const Layout = ({children, containerClassName}) => (
-  <div className='flex flex-column min-vh-100'>
+  <div className='flex flex-column flex-auto min-vh-100'>
     <Header />
     <main className={'flex-auto ' + (containerClassName || 'ph3 ph5-ns pv5')}>
       {children}
